040c70b4c7cf3b44e619b16d5f679dfe84f541fe
RasmusLumholdt <dev.rlumholdt@gmail.com>
2019-02-17 21:34:19 +0100

    Added csv to dict

--- csv_to_dic.py
+++ csv_to_dic.py
@@ -1,11 +1,35 @@
 import urllib.request as urllib
-import csv
 
 
-url = 'http://data.kk.dk/dataset/76ecf368-bf2d-46a2-bcf8-adaf37662528/resource/9286af17-f74e-46c9-a428-9fb707542189/download/befkbhalderstatkode.csv'
+url = 'data.csv'
+
+def createDict():
+        statistics = {}
+        with open(url) as f:
+                lines = f.readlines()
+                
+                for i in range(1, len(lines)):
+                        #Handle data and make it into arrays
+                        line = lines[i]
+                        values = list(map(int, line.split(",")))
+                        currDict = statistics
+
+                        for y in range(3):
+                                #Set current value
+                                currVal = values[y]
+
+                                #If it exist in the structure:
+                                if currVal in statistics.keys():
+                                        #Go to next level of dictionary
+                                        currDict = currDict.get(currVal)
+
+                                #If it doesn't
+                                else:
+                                        #Add value as dict
+                                        currDict[currVal] = {}
+                                        currDict = currDict[currVal]
+                        #Set last k/v pair
+                        currDict[values[3]] = values[4]
 
 
-with urllib.urlopen(url) as dlFile:
-        data = dlFile.read().decode('utf-8')
-        cleanData = data.strip("\r")
-        print(type(cleanData))
+createDict()
